4d92a04846c650a1b83505553befdd3bd3488a49
Tarster <sharmatarun537@gmail.com>
2021-08-09 16:39:49 +0530

    Updated Homepage file

---
+++ lib/app/home/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_final/app/home/cupertino_home_scaffold.dart';
+import 'package:time_tracker_final/app/home/tab_item.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TabItem _currentTab = TabItem.jobs;
+  void _select(TabItem tabItem) {
+  setState(() =>_currentTab = tabItem);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoHomeScaffold(
+        currentTab: _currentTab, onSelectTab: _select);
+  }
+
+
+}
